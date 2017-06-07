349f82010617ac4fc9b4d376ddd05f89fec2b19f
ascrutae <nzt48.xin@gmail.com>
2017-06-07 13:07:58 +0800

    push image

--- travis/push_image.sh
+++ travis/push_image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-IMAGE_VERSION=3.0.2-2017
+IMAGE_VERSION=3.0.3-2017
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 mvn clean package docker:build
 docker push skywalking/skywalking-collector:latest
